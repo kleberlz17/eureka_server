7da7bb4b6c81398cba6abac4b5675d8e4ed8c8d3
Kleber Luiz <157175058+kleberlz17@users.noreply.github.com>
2025-06-16 22:41:34 -0300

    Substituindo projeto antigo por novo

---
+++ src/test/java/kleberlz/eurekanovo/EurekanovoApplicationTests.java
@@ -0,0 +1,13 @@
+package kleberlz.eurekanovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekanovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
